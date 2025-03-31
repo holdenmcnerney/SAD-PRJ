8f5b94cf84057df05e2b873af199297db0a7dad0
holdenmcnerney <holdenmcnerney@gmail.com>
2025-03-30 20:21:05 -0600

    training is running but not working

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 # AEM 525 Project Holden McNerney
 
-from quaternionpointing import QuaternionPointingEnv
+from quaternionpointing_env import QuaternionPointingEnv
+from quaternionpointing_ppo_training import QuaternionPointingTraining
 
-import numpy as np
-from typing import Optional
 from collections import defaultdict
+import numpy as np
+import matplotlib.pyplot as plt
 
 import torch
 from torch import nn
+from torch import multiprocessing
 import tqdm
 from tensordict import TensorDict, TensorDictBase
 from tensordict.nn import TensorDictModule
 
-from torchrl.data import Bounded, Composite, Unbounded
-from torchrl.envs import EnvBase
+from torchrl.envs import (
+    CatTensors,
+    TransformedEnv,
+    Compose,
+    StepCounter,
+)
 
 from torchrl.envs.utils import check_env_specs, step_mdp
-from torch.linalg import norm
 
 def simple_rollout(env, steps=100):
     # preallocate:
@@ -25,24 +30,26 @@ def simple_rollout(env, steps=100):
     _data = env.reset()
     for i in range(steps):
         _data["action"] = env.action_spec.rand()
+        # print(_data["action"])
         _data = env.step(_data)
         data[i] = _data
         _data = step_mdp(_data, keep_other=True)
     return data
 
 def plot(logs):
-    import matplotlib
-    from matplotlib import pyplot as plt
-
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(logs["return"])
-    plt.title("returns")
-    plt.xlabel("iteration")
-    plt.subplot(1, 2, 2)
-    plt.plot(logs["last_reward"])
-    plt.title("last reward")
-    plt.xlabel("iteration")
+    plt.figure(figsize=(10, 10))
+    plt.subplot(2, 2, 1)
+    plt.plot(logs["reward"])
+    plt.title("training rewards (average)")
+    plt.subplot(2, 2, 2)
+    plt.plot(logs["step_count"])
+    plt.title("Max step count (training)")
+    plt.subplot(2, 2, 3)
+    plt.plot(logs["eval reward (sum)"])
+    plt.title("Return (test)")
+    plt.subplot(2, 2, 4)
+    plt.plot(logs["eval step_count"])
+    plt.title("Max step count (test)")
     plt.show()
 
 def training(env):
@@ -56,7 +63,7 @@ def training(env):
         nn.Tanh(),
         nn.LazyLinear(64),
         nn.Tanh(),
-        nn.LazyLinear(1),
+        nn.LazyLinear(3),
     )
     policy = TensorDictModule(
         net,
@@ -64,13 +71,13 @@ def training(env):
         out_keys=["action"],
     )
     optim = torch.optim.Adam(policy.parameters(), lr=2e-3)
-    batch_size = 32
-    pbar = tqdm.tqdm(range(20_000 // batch_size))
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, 20_000)
+    # batch_size = 32
+    pbar = tqdm.tqdm(range(200))
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, 200)
     logs = defaultdict(list)
 
     for _ in pbar:
-        init_td = env.reset(env.gen_params(batch_size=[batch_size]))
+        init_td = env.reset(env.gen_params())
         rollout = env.rollout(100, policy, tensordict=init_td, auto_reset=False)
         traj_return = rollout["next", "reward"].mean()
         (-traj_return).backward()
@@ -79,22 +86,40 @@ def training(env):
         optim.zero_grad()
         pbar.set_description(
             f"reward: {traj_return: 4.4f}, "
-            f"last reward: {rollout[..., -1]['next', 'reward'].mean(): 4.4f}, gradient norm: {gn: 4.4}"
+            f"last reward: {rollout[..., -1]['next', 'reward'].mean(): 4.4f}, \
+              gradient norm: {gn: 4.4}"
         )
         logs["return"].append(traj_return.item())
         logs["last_reward"].append(rollout[..., -1]["next", "reward"].mean().item())
         scheduler.step()
 
     plot(logs)
+    return rollout
 
 def main():
 
-    env = QuaternionPointingEnv()
+    is_fork = multiprocessing.get_start_method() == "fork"
+    device = (
+        torch.device(0)
+        if torch.cuda.is_available() and not is_fork
+        else torch.device("cpu")
+    )
+    env = QuaternionPointingEnv(device=device)
+    env = TransformedEnv(
+        env,
+        Compose(
+            StepCounter(),
+            CatTensors(
+                in_keys=["q", "q_dot", "omega", "q_d"], dim=1, 
+                out_key="observation", del_keys=False, sort=False
+            ),
+        ),
+    )
     check_env_specs(env)
 
-    # batch_size = 10  # number of environments to be executed in batch
-    # td = env.reset(env.gen_params(batch_size=[batch_size]))
-    # print("reset (batch size of 10)", td)
+    logs = QuaternionPointingTraining(env)
+
+    plot(logs)
 
     return 0
 
--- quaternionpointing_env.py
+++ quaternionpointing_env.py
@@ -8,7 +8,9 @@ from torchrl.data import Bounded, Composite, Unbounded
 from torchrl.envs import EnvBase
 from torch.linalg import norm
 
-THETA_LIM = 10*np.pi/180
+from torch import multiprocessing
+
+THETA_LIM = 20*np.pi/180
 
 def _step(tensordict):
     ''''
@@ -21,24 +23,27 @@ def _step(tensordict):
     Iyy = tensordict["params", "Iyy"]
     Izz = tensordict["params", "Izz"]
     dt = tensordict["params", "dt"]
-    M = tensordict["action"].squeeze(-1)
+    M = tensordict["action"]
     M = M.clamp(-tensordict["params", "max_moment"], 
                  tensordict["params", "max_moment"])
-    costs = torch.square(norm(q - q_d)) + a_1*torch.square(norm(q_dot)) \
-                                        + a_2*torch.square(norm(M))
+    q = torch.transpose(q, 0, 1)
+    omega = torch.transpose(omega, 0, 1)
+    M = torch.transpose(M, 0, 1)
+    q_d = torch.transpose(q_d, 0, 1)
+    costs = -(norm(q - q_d) + a_1*norm(q_dot) \
+                          + a_2*norm(M))
     
     I = torch.tensor(([Ixx, 0, 0], [0, Iyy, 0], [0, 0, Izz]), 
-                     dtype=torch.float32)
+                     dtype=torch.float32, device='cuda:0')
     old_omega_1, old_omega_2, old_omega_3 = omega[0], \
                                             omega[1], \
                                             omega[2] 
     omega_skew = torch.tensor(([0, -old_omega_3, old_omega_2], 
                                [old_omega_3, 0, -old_omega_1], 
                                [-old_omega_2, old_omega_1, 0]), 
-                               dtype=torch.float32)
+                               dtype=torch.float32, device='cuda:0')
 
-    omega_dot_inner = torch.transpose(M, 0, 1) \
-                      - torch.mm(torch.mm(omega_skew, I), omega)
+    omega_dot_inner = M - torch.mm(torch.mm(omega_skew, I), omega)
     omega_dot = torch.mm(torch.transpose(I, 0, 1), omega_dot_inner)
     new_omega = omega + omega_dot*dt
     new_omega_1, new_omega_2, new_omega_3 = new_omega[0], \
@@ -48,13 +53,18 @@ def _step(tensordict):
                               [-new_omega_3, 0, new_omega_1, new_omega_2], 
                               [new_omega_2, -new_omega_1, 0, new_omega_3], 
                               [-new_omega_1, -new_omega_2, -new_omega_1, 0]), 
-                              dtype=torch.float32)
+                              dtype=torch.float32, device='cuda:0')
     new_q_dot = 1/2*torch.mm(Omega_mat, q)
     new_q = q + new_q_dot*dt
     new_q = torch.nn.functional.normalize(new_q, dim=0)
 
     reward = -costs.view(*tensordict.shape, 1)
     done = torch.zeros_like(reward, dtype=torch.bool)
+
+    new_q = torch.transpose(new_q, 0, 1)
+    new_q_dot = torch.transpose(new_q_dot, 0, 1)
+    new_omega = torch.transpose(new_omega, 0, 1)
+    q_d = torch.transpose(q_d, 0, 1)
     out = TensorDict(
         {
             "q": new_q,
@@ -74,21 +84,19 @@ def _reset(self, tensordict):
         tensordict = self.gen_params(batch_size=self.batch_size)
 
     # Randomly start the simulation values. Starting with small angles
-    q = torch.tensor(([0], [0], [0], [1]), dtype=torch.float32)
-    q_dot = torch.tensor(([0], [0], [0], [0]), dtype=torch.float32)
-    omega = torch.tensor(([0], [0], [0]), dtype=torch.float32)
+    q = torch.tensor(([[0, 0, 0, 1]]), dtype=torch.float32, device=self.device)
+    q_dot = torch.tensor(([[0, 0, 0, 0]]), dtype=torch.float32, device=self.device)
+    omega = torch.tensor(([[0, 0, 0]]), dtype=torch.float32, device=self.device)
 
-    rot_axis = torch.rand(3, generator=self.rng, device=self.device)
+    rot_axis = torch.rand(3, generator=torch.Generator(device='cuda'), device=self.device)
     rot_axis = torch.nn.functional.normalize(rot_axis, dim=0)
-    theta = torch.rand(1, generator=self.rng, device=self.device)
+    theta = torch.rand(1, generator=torch.Generator(device='cuda'), device=self.device)
     # THETA_LIM is set to 10 degrees to start
     theta = theta * THETA_LIM
-    q_d_1_3 = torch.tensor(([rot_axis[0]*torch.sin(theta/2)], 
-                            [rot_axis[1]*torch.sin(theta/2)], 
-                            [rot_axis[2]*torch.sin(theta/2)]), 
-                            dtype=torch.float32)
-    q_d_4 = torch.tensor(([torch.cos(theta/2)]), dtype=torch.float32).view(1, 1)
-    q_d = torch.cat((q_d_1_3, q_d_4), 0) 
+    q_d = torch.tensor(([[rot_axis[0]*torch.sin(theta/2), 
+                          rot_axis[1]*torch.sin(theta/2), 
+                          rot_axis[2]*torch.sin(theta/2),
+                          torch.cos(theta/2)]]), dtype=torch.float32, device=self.device)
 
     out = TensorDict(
         {
@@ -107,26 +115,30 @@ def _make_spec(self, td_params):
         q=Bounded(
             low=-td_params["params", "max_q"],
             high=td_params["params", "max_q"],
-            shape=(4,1),
+            shape=torch.Size([1, 4]),
             dtype=torch.float32,
+            device=self.device,
         ),
         q_dot=Bounded(
             low=-td_params["params", "max_q_dot"],
             high=td_params["params", "max_q_dot"],
-            shape=(4,1),
+            shape=torch.Size([1, 4]),
             dtype=torch.float32,
+            device=self.device,
         ),
         omega=Bounded(
             low=-td_params["params", "max_omega"],
             high=td_params["params", "max_omega"],
-            shape=(3,1),
+            shape=torch.Size([1, 3]),
             dtype=torch.float32,
+            device=self.device,
         ),
         q_d=Bounded(
             low=-td_params["params", "max_q"],
             high=td_params["params", "max_q"],
-            shape=(4,1),
+            shape=torch.Size([1, 4]),
             dtype=torch.float32,
+            device=self.device,
         ),
         params = make_composite_from_td(td_params["params"]),
         shape=(),
@@ -135,10 +147,11 @@ def _make_spec(self, td_params):
     self.action_spec = Bounded(
         low=-td_params["params", "max_moment"],
         high=td_params["params", "max_moment"],
-        shape=(1,3),
+        shape=torch.Size([1, 3]),
         dtype=torch.float32,
+        device=self.device,
     )
-    self.reward_spec = Unbounded(shape=(*td_params.shape, 1))
+    self.reward_spec = Unbounded(shape=(*td_params.shape, 1), device=self.device)
 
 def make_composite_from_td(td):
     composite = Composite(
@@ -159,6 +172,12 @@ def _set_seed(self, seed: Optional[int]):
     self.rng = rng
 
 def gen_params(batch_size=None) -> TensorDictBase:
+    is_fork = multiprocessing.get_start_method() == "fork"
+    device = (
+        torch.device(0)
+        if torch.cuda.is_available() and not is_fork
+        else torch.device("cpu")
+    )
     if batch_size is None:
         batch_size = []
     td = TensorDict(
@@ -167,19 +186,19 @@ def gen_params(batch_size=None) -> TensorDictBase:
                 {
                     "max_q": 1,
                     "max_q_dot": 1,
-                    "max_omega": 1,
-                    "max_moment": 1,
+                    "max_omega": 0.5,
+                    "max_moment": 0.005,
                     "a_1": 0.01,
                     "a_2": 0.001,
-                    "Ixx": 1,
-                    "Iyy": 1,
-                    "Izz": 1,
+                    "Ixx": 30,
+                    "Iyy": 30,
+                    "Izz": 10,
                     "dt": 0.01,
                 },
                 [],
-            )
+            ),
         },
-        [],
+        device=device,
     )
     if batch_size:
         td = td.expand(batch_size).contiguous()
